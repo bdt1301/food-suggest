f97d37d59372d5ba50ddb1b8b9dc43e9780ba2d1
bdt1301 <thienphu1301@gmail.com>
2026-01-12 13:36:20 +0700

    refactor: rename table to users_foodsuggest to avoid database conflicts

--- src/main/java/com/user/foodsuggest/model/User.java
+++ src/main/java/com/user/foodsuggest/model/User.java
@@ -13,7 +13,7 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
-@Table(name = "users")
+@Table(name = "users_foodsuggest")
 public class User {
 
     @Id
