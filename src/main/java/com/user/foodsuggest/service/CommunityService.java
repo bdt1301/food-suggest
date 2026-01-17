f0ba7f3d280235cda0d5b986bbd56953a69466b3
bdt1301 <thienphu1301@gmail.com>
2026-01-17 08:46:30 +0700

    fix: prevent dropdown from expanding navbar on mobile

--- src/main/java/com/user/foodsuggest/service/CommunityService.java
+++ src/main/java/com/user/foodsuggest/service/CommunityService.java
@@ -1,12 +1,7 @@
 package com.user.foodsuggest.service;
 
-import java.util.Optional;
-
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.security.authentication.AnonymousAuthenticationToken;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
 import com.user.foodsuggest.dto.CloneDishDTO;
